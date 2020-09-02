c76d30624c409fb7a5ee240d38a2b38a9fd956f7
Uditanshu Pandey <60996223+Uditanshu0110@users.noreply.github.com>
2020-09-03 03:22:23 +0530

    Added Files

---
+++ aws_rds.tf
@@ -0,0 +1,22 @@
+provider "aws"{
+  profile ="default"
+  region ="ap-south-1"
+}
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  identifier           = "maria-db"
+  engine               = "mariadb"
+  engine_version       = "10.4"
+  instance_class       = "db.t2.micro"
+  name                 = "myrdsdb"
+  username             = "uditanshupandey"
+  password             = "redhat123"
+  parameter_group_name = "default.mariadb10.4"
+  skip_final_snapshot  = true
+  #iam_database_authentication_enabled = true
+  publicly_accessible = true
+  tags = {
+    Name = "myrdsdb"
+  }
+}
